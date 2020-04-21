74e8ad1207bcad6fdd5d3ecf6a94e097f30c21e0
AndryKazanovsky <kazanovsky.andrew@gmail.com>
2020-04-21 14:56:02 +0300

    alp-test

--- AlphaPharmaceutical/send.php
+++ AlphaPharmaceutical/send.php
@@ -3,25 +3,23 @@
 /* set variables */
 $name = htmlspecialchars($_POST["name"]);
 $messangerContact = htmlspecialchars($_POST["messanger-contact"]);
-$messanger = htmlspecialchars($_POST["messanger"]);
 $message = htmlspecialchars($_POST["message"]);
 
 /* Your address and subject line */
-$address = "popovdenys.web@gmail.com";
-$sub = "Сообщение с сайта denyspopov.com";
+$address = "for23motor@gmail.com";
+$sub = "Сообщение с сайта AP";
 
 /* form format */
-$mes = "Сообщение с сайта denyspopov.com.\n
+$mes = "Сообщение с сайта AP\n
 Name: $name
 Messanger Contact: $messangerContact
-Messanger: $messanger
 Message: $message";
 
 /* send message use mail()  */
 $from = ('From:<'. $address .'> ');
 
 if (mail($address, $sub, $mes, $from)) {
-    header('Refresh: 5; URL=https://denyspopov.com');
+    header('Refresh: 5; URL=https://andrykazanovsky.github.io/testview.github.io/AlphaPharmaceutical/index.html');
     echo 'ok';}
 else {
-    header('Refresh: 5; URL=https://denyspopov.com');
+    header('Refresh: 5; URL=https://andrykazanovsky.github.io/testview.github.io/AlphaPharmaceutical/index.html');
