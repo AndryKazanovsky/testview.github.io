b468073ff104f86599c6f5c113582dbfa4f401f2
AndryKazanovsky <kazanovsky.andrew@gmail.com>
2020-04-21 15:01:02 +0300

    alp-test

--- AlphaPharmaceutical/js/script-pl.js
+++ AlphaPharmaceutical/js/script-pl.js
@@ -18,6 +18,29 @@
     }
 
 
+      /////// send form
+      let button = document.querySelector('button');
+
+      /*add event on the send button*/
+      button.addEventListener('click', function(event) {
+          event.preventDefault();
+          /*create object*/
+          let formData = {
+              name: document.querySelector('#name').value,
+              messangerContact: document.querySelector('#messanger-contact').value,
+              message: document.querySelector('#message').value
+          }
+
+          /*transmit data*/
+          let request = new XMLHttpRequest();
+
+          request.open('POST', "send.php")
+          request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded; charset=UTF-8');
+          request.send('name=' + encodeURIComponent(formData.name) + '&messangerContact=' + encodeURIComponent(formData.messangerContact) + '&message=' +  encodeURIComponent(formData.message))
+      })
+
+
+      console.log('Im Here!');
 
 // var controller = new ScrollMagic.Controller();
 //
