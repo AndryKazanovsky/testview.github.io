2d0d3786f3846522cda4f18f176c78ab63e6b1ae
AndryKazanovsky <kazanovsky.andrew@gmail.com>
2021-05-09 12:30:09 +0300

    inf-kz

--- Lazea-new/js/script.js
+++ Lazea-new/js/script.js
@@ -1,11 +1,39 @@
 (function() {
   window.onload = function() {
 
-const alertLicense = document.querySelector('.alert-license');
-    const licenseOff = document.querySelector('.license-off');
-      licenseOff.onclick = function() {
-          alertLicense.classList.add('hide');
+    // const alertLicense = document.querySelector('.alert-license');
+    // const licenseOff = document.querySelector('.license-off');
+    //   licenseOff.onclick = function() {
+    //       alertLicense.classList.add('hide');
+    //   };
+
+    (function() {
+      const inpt = document.querySelector(".this-inpt");
+      const menu1 = document.querySelector(".menuLi1");
+      const menu2 = document.querySelector(".menuLi2");
+      const menu3 = document.querySelector(".menuLi3");
+      const menu4 = document.querySelector(".menuLi4");
+      const menu5 = document.querySelector(".menuLi5");
+
+      menu1.onclick = function() {
+        inpt.click();
       };
+      menu2.onclick = function() {
+        inpt.click();
+      };
+      menu3.onclick = function() {
+        inpt.click();
+      };
+      menu4.onclick = function() {
+        inpt.click();
+      };
+      menu5.onclick = function() {
+        inpt.click();
+      };
+
+    })
+
+
 
     (function() {
 
